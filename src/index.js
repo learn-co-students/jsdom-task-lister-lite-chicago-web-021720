2a81d16e2052a775099c489ca411effe53797a70
cooljasonmelton <cooljasonmelton>
2020-03-31 13:32:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasks = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskVal = document.getElementById('new-task-description').value
+
+    const taskLi = document.createElement('li');
+
+    const taskButton = document.createElement('button')
+    taskButton.innerText = 'X'
+
+    taskLi.innerText = taskVal;
+    taskLi.appendChild(taskButton);
+    tasks.appendChild(taskLi);
+
+    taskButton.addEventListener('click', function() {
+      taskLi.remove();
+    });
+  });
 });
+
